a2fcf3b3f8d9fd98b878b76b5ee792d56ff5a255
kukushkinam <133341363+kukushkinam@users.noreply.github.com>
2023-05-20 17:12:15 +0300

    start

---
+++ sdown.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#завершение работы удаленного сервера по ssh
+
+#ПАРАМЕТРЫ
+#-a после выполнения выключает хост
+
+#НАСТРОЙКА ПОДКЛЮЧЕНИЯ
+#создать ключи ssh 
+#ssh keygen
+#отправить открытый ключ на управляемую машину
+#ssh-copy-id username@ip
+#добавить username@ip в переменную servers
+
+#список серверов username@ip
+servers=(
+"vagrant@192.168.0.27"
+"vagrant@192.168.0.65"
+)
+#команда завершения работы
+comand="sudo shutdown -P now"
+
+for server in ${servers[*]} 
+  do
+    echo "ssh $server $comand"
+    ssh $server $comand
+  done
+
+#преверим наличие параметра для выключения хоста
+if [ "$1" = "-a" ] 
+  then
+    echo $comand
+    $comand
+fi
